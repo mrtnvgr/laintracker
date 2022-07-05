b6e327e48f95e0ca696a182ffed1501ac62bc11a
mrtnvgr <martynovegorOF@yandex.ru>
2022-07-05 18:01:12 +0700

    gui tracker init

---
+++ laintracker/__main__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from .tracker.main import Tracker
+
+def main():
+    Tracker()
+
+if __name__=="__main__":
+    main()
---
+++ laintracker/tracker/main.py
@@ -0,0 +1,3 @@
+class Tracker:
+    def __init__(self):
+        print("Hello world!")
